dcfe769b80cb1aa98de31fb09c4e9344e9992533
Rakesh <rakeshabhinav65@gmail.com>
2022-09-20 12:59:45 +0530

    updated js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var initialPrice = document.querySelector('#ntial-price');
+var initialPrice = document.querySelector('#initial-price');
 var stockQuantity = document.querySelector('#stocks-quantity');
 var currentPrice = document.querySelector('#current-price');
 var submitBtn = document.querySelector('#submit-btn');
@@ -7,9 +7,9 @@ var outputBox = document.querySelector('#output-box');
 submitBtn.addEventListener('click',submitHandler);
 
 function submitHandler(){
-    var ip = (initialPrice.value);
-    var qty = (stockQuantity.value);
-    var curr = (currentPrice.value);
+    var ip = Number(initialPrice.value);
+    var qty = Number(stockQuantity.value);
+    var curr = Number(currentPrice.value);
 
     calaculateProfitAndLoss(ip,qty,curr);
 }
@@ -18,25 +18,22 @@ function submitHandler(){
 
 function calaculateProfitAndLoss(initial, quantity, current){
     if(initial > current){
-        var loss = ((initial - current) * quantity).toFixed(2);
-        var lossPercentage = Math.trunc((loss / initial) * 100) ;
+        var loss = ((initial - current) * quantity);
+        var lossPercentage = Math.trunc((loss / initial) * 100).toFixed(2) ;
 
-        showOutput(`the loss is ${loss} and the losspercent is ${lossPercentage}%`);
+        showOutput(`the loss is ${loss}% and the loss-percent is ${lossPercentage}%`);
     }
     else if(current > initial ){
-         var profit = ((current - initial) * quantity).toFixed(2);
-         var profitPercentage = Math.trunc((profit / initial) * 100);
+         var profit = ((current - initial) * quantity);
+         var profitPercentage = Math.trunc((profit / initial) * 100).toFixed(2);
 
-         showOutput(`the profit is ${profit} and the profitpercentage is ${profitPercentage}`);
+         showOutput(`the profit is ${profit}% and the profit-percentage is ${profitPercentage}%`);
     }
     else {
         showOutput(`No pain no gain no gain no pain`);
 
-
     }
 }
-
-
 
 function showOutput(message){
     
